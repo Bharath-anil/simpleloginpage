0ca1e691f446ba10fb919719b95f2973c0b160d0
Bharath M A <bharathanil.it@gmail.com>
2023-07-05 16:17:25 +0530

    edited login and signup form contain aalignment issue

--- login.js
+++ login.js
@@ -1,6 +1,6 @@
 const formOpenBtn = document.querySelector("#form-open"),
 home= document.querySelector(".home"),
-formContainer = document.querySelector(".form_container"),
+formContainer = document.querySelector(".form-container"),
 formCloseBtn = document.querySelector(".form-close"),
 signupBtn = document.querySelector("#signup")
 loginBtn = document.querySelector("#login")
@@ -9,13 +9,13 @@ loginBtn = document.querySelector("#login")
 formOpenBtn.addEventListener("click", () =>home.classList.add('show'));
 formCloseBtn.addEventListener("click", () =>home.classList.remove('show'));
 
-signupBtn.addEventListener("click",(e) =>{
+signupBtn.addEventListener("click", (e) => {
     e.preventDefault();
-    formContainer.classList.add('active');
+    formContainer.classList.add("active");
 
 });
 loginBtn.addEventListener("click",(e) =>{
     e.preventDefault();
-    formContainer.classList.remove('active');
+    formContainer.classList.remove("active");
 
 });
